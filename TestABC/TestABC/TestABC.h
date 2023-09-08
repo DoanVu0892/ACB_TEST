a38b2080ccd3ac91fcec7553d40ac490603faac0
DoanVu0892 <johncater.0892@gmail.com>
2023-09-08 11:34:36 +0700

    add TestABC

---
+++ TestABC/TestABC/TestABC.h
@@ -0,0 +1,18 @@
+//
+//  TestABC.h
+//  TestABC
+//
+//  Created by Đoàn Vũ on 08/09/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestABC.
+FOUNDATION_EXPORT double TestABCVersionNumber;
+
+//! Project version string for TestABC.
+FOUNDATION_EXPORT const unsigned char TestABCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestABC/PublicHeader.h>
+
+
